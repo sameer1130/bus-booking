fc166e37a0b1e21b13df5d35a0537622c338c389
Sameer Kamble <sameerkamble1130@gmail.com>
2025-05-06 12:22:01 +0530

    backend settings and urls

--- backend/settings.py
+++ backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -40,6 +41,7 @@ INSTALLED_APPS = [
     'bus',
     'core',
     'booking',
+    'destinations',
     'rest_framework',
     'rest_framework_simplejwt',
     'django_filters',
@@ -141,3 +143,8 @@ SIMPLE_JWT = {
     'USER_ID_FIELD': 'uuid',
     'USER_ID_CLAIM': 'user_uuid',
 }
+
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
